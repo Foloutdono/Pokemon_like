3496bb4d4efa51a2651867b363749dc57409b3b9
Foloutdono <125478727+Foloutdono@users.noreply.github.com>
2025-09-15 16:15:46 +0200

    Delete game directory

--- game/poke/poke_type.py
+++
@@ -1,8 +0,0 @@
-class PokeType():
-    def __init__(self, name, id, attack_coefs):
-        self.name = name
-        self.id = id
-        self.attack_coefs = attack_coefs
-
-    def __str__(self):
-        return f'Type : {self.name}'
