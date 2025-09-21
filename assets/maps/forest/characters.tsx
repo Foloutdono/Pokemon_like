9071431ef18ac7b29e74b68a19ffc6f5f1656837
Foloutdono <125478727+Foloutdono@users.noreply.github.com>
2025-09-21 16:28:35 +0200

    Delete assets/maps/forest directory

--- assets/maps/forest/characters.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="characters" tilewidth="16" tileheight="16" tilecount="400" columns="20">
- <image source="../Pokemon like/assets/tilesets/forest/characters.png" width="320" height="320"/>
-</tileset>
